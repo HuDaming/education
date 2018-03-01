923fda3d598d87a1f018a4aa7f6584eb343f117a
Hudaming <40834106@qq.com>
2018-03-01 07:02:37 +0000

    Fimish static pagas

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Education App') - 5U足球教育系统</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
