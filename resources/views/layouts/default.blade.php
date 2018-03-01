0e3667e0e92ef0fe0fda638b56d2a2725d4d033d
Hudaming <40834106@qq.com>
2018-03-01 08:14:54 +0000

    Fimish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,17 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Education App') - 5U足球教育系统</title>
+    <title>@yield('title', 'U教育') - 5U足球教育系统</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
 </body>
 </html>
