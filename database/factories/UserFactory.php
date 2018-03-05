e2314447db01dc741ea8f318da6c29786c7c950d
Hudaming <40834106@qq.com>
2018-03-05 01:55:26 +0000

    Add account activations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,15 +14,16 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
-    static $password;
     $date_time = $faker->date . ' ' . $faker->time;
+    static $password;
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'is_admin' => false,
+        'activated' => true,
         'password' => $password ?: $password = bcrypt('secret'), // secret
         'remember_token' => str_random(10),
-        'is_admin' => false,
         'created_at' => $date_time,
         'updated_at' => $date_time,
     ];
